035f9a59d619333cd9ea8aad1311cc6a33fd98db
zGonzq <gonzaloandres2508@gmail.com>
2024-11-11 20:55:49 -0300

    Ban created

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,56 +1,76 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
-module.exports = {
+const {
+    Client,
+    Interaction,
+    ApplicationCommandOptionType,
+    PermissionFlagsBits,
+    EmbedBuilder,
+  } = require('discord.js');
+  
+  module.exports = {
+    /**
+     *
+     * @param {Client} client
+     * @param {Interaction} interaction
+     */
+  
+    callback: async (client, interaction) => {
+
+      const embed = new EmbedBuilder()
+
+      const targetUserId = interaction.options.get('target-user').value;
+      const reason =
+        interaction.options.get('reason')?.value || 'No se especificó una razón.';
+  
+  
+      const targetUser = await interaction.guild.members.fetch(targetUserId);
+  
+      if (!targetUser) {
+        await interaction.reply({ embeds: [embed.setDescription(`El usuario ${targetUser.name} no existe.`).setColor('Red').setTitle('Error')] });
+        return;
+      }
+  
+      if (targetUser.id === interaction.guild.ownerId) {
+        await interaction.reply({ embeds: [embed.setDescription(`No puedes banear al dueño del servidor.`).setColor('Red').setTitle('Error')] });
+        return;
+      }
+  
+      const targetUserRolePosition = targetUser.roles.highest.position; 
+      const requestUserRolePosition = interaction.member.roles.highest.position; 
+      const botRolePosition = interaction.guild.members.me.roles.highest.position; 
+  
+      if (targetUserRolePosition >= requestUserRolePosition) {
+        await interaction.reply({ embeds: [embed.setDescription(`No puedes banear a alguien con un rol igual o superior al tuyo.`).setColor('Red').setTitle('Error')] });
+        return;
+      }
+  
+      if (targetUserRolePosition >= botRolePosition) {
+        await interaction.reply({ embeds: [embed.setDescription(`No puedo banear a alguien con un rol igual o superior al mío.`).setColor('Red').setTitle('Error')] });
+        return;
+      }
+
+      try {
+        await targetUser.ban({ reason });
+        await interaction.reply({ embeds: [embed.setDescription(`El usuario ${targetUser.user.tag} ha sido baneado.`).setColor('Green').setTitle('Baneado')] });
+      } catch (error) {
+        console.log(`There was an error when banning: ${error}`);
+      }
+    },
+  
     name: 'ban',
-    description: 'Banea a un usuario.',
-    // devOnly: true,
-    // testOnly: true,
+    description: 'Banea a un usuario del servidor.',
     options: [
-        {
-            name: 'usuario',
-            description: 'El usuario que quieres banear.',
-            required: true,
-            type: ApplicationCommandOptionType.Mentionable
-        },
-        {
-            name: 'razon',
-            description: 'La razón por la que quieres banear a este usuario.',
-            required: false,
-            type: ApplicationCommandOptionType.String
-        }
+      {
+        name: 'target-user',
+        description: 'El usuario que quieres banear.',
+        type: ApplicationCommandOptionType.Mentionable,
+        required: true,
+      },
+      {
+        name: 'reason',
+        description: 'La razón por la que quieres banear al usuario.',
+        type: ApplicationCommandOptionType.String,
+      },
     ],
-
-    permissions: [PermissionFlagsBits.BanMembers],
-    botPermissions: [PermissionFlagsBits.Administrator],
-
-    callback: (client, interaction) => {
-
-        const user = interaction.options.get('usuario').user;
-        const reason = interaction.options.get('razon')?.value || 'No se especificó una razón.';
-
-        const embed = new EmbedBuilder()
-
-        if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) return interaction.reply({ content: 'No tienes permisos para banear a un usuario.', ephemeral: true });
-
-        if (!interaction.guild.me.permissions.has(PermissionFlagsBits.BanMembers)) 
-
-        if(!user) return interaction.reply({ content: 'No se pudo encontrar al usuario.', ephemeral: true });
-
-        if(user.id === interaction.guild.ownerId) return interaction.reply({ content: 'No puedo banear al dueño del servidor.', ephemeral: true });
-
-        if(user.id === client.user.id) return interaction.reply({ content: 'No puedo banearme a mi mismo.', ephemeral: true });
-
-        if(user.id === interaction.member.id) return interaction.reply({ content: 'No puedes banearte a ti mismo.', ephemeral: true });
-
-        if (interaction.member.roles.highest.position <= user.roles.highest.position) return interaction.reply({ content: 'No puedes banear a alguien con un rol igual o superior al tuyo.', ephemeral: true });
-
-        if (user.bot) return interaction.reply({ content: 'No puedo banear a un bot.', ephemeral: true });
-
-        try {
-            interaction.guild.members.ban(user, { reason });
-            interaction.reply({ content: `**${user.tag}** ha sido baneado.`, ephemeral: true });
-        } catch (error) {
-            interaction.reply({ content: 'Ocurrió un error al intentar banear al usuario.', ephemeral: true });
-            console.error(error);
-        }
-    }
-}
+    permissionsRequired: [PermissionFlagsBits.BanMembers],
+    botPermissions: [PermissionFlagsBits.BanMembers],
+  };
