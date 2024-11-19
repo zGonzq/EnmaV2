3d22326e884df1b184df67be3da1dcf00016d891
zGonzq <gonzaloandres2508@gmail.com>
2024-11-19 01:09:06 -0300

    fix?

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-const {Client, IntentsBitField, ActivityType} = require('discord.js');
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
 const mongoose = require('mongoose');
-require('dotenv').config()
 
 const client = new Client({
-    intents: [ IntentsBitField.Flags.Guilds,
-                IntentsBitField.Flags.GuildMessages,
-                IntentsBitField.Flags.GuildMembers,
-                IntentsBitField.Flags.MessageContent,
-     ]
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.MessageContent,
+    ],
 });
-
 
 (async () => {
     try {
-        eventHandler(client)
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('🛫 Conectado a la base de datos')
+        eventHandler(client);
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('🛫 Conectado a la base de datos');
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-})()
-
+})();
 
 client.login(process.env.BOT_TOKEN).then(() => {
-    client.user.setActivity({name: '🤔', type: ActivityType.Watching})
-})
+    client.user.setActivity({ name: '🤔', type: ActivityType.Watching });
+});
