bc0ec13f52fdc0c99eda43507e73c0143c076119
zGonzq <gonzaloandres2508@gmail.com>
2024-11-19 00:32:33 -0300

    mongo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const {Client, IntentsBitField, ActivityType} = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 const client = new Client({
@@ -10,7 +11,17 @@ const client = new Client({
      ]
 });
 
-eventHandler(client);
+
+(async () => {
+    try {
+        eventHandler(client)
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('🛫 Conectado a la base de datos')
+    } catch (error) {
+        console.log(error)
+    }
+})()
+
 
 client.login(process.env.BOT_TOKEN).then(() => {
     client.user.setActivity({name: '🤔', type: ActivityType.Watching})
